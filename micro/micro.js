2367d78c06d4e2063c3e63d0b4423fcf4e274f05
ciciDawgy <108370999+ciciDawgy@users.noreply.github.com>
2022-08-17 17:24:46 -0400

    Add files via upload

---
+++ micro/micro.js
@@ -0,0 +1,17 @@
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+const btna = document.querySelector('.btna')
+const input = document.querySelector('.input')
+
+btna.addEventListener('click', () => {
+    search.classList.toggle('active')
+    input.focus()
+}) 
