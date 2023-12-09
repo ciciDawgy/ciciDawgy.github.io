a113f5a8d4ffa844516d20f1ce16c57fc9d77f26
ciciDawgy <108370999+ciciDawgy@users.noreply.github.com>
2023-12-09 07:15:08 -0500

    Update script.js

--- spaceOneOne-main/script.js
+++ spaceOneOne-main/script.js
@@ -1,10 +1,5 @@
 const rocket = document.querySelector('.rocket');
-const active = document.querySelector('.active');
-var modal = document.querySelector('.modal');
-const removeModal = document.querySelector('.fa-solid');
 const planets = document.querySelectorAll('.planet');
-const Neptune = document.querySelector('.neptune');
-/*var elementClassName = e.target.className;*/
 
 
 function showRocket() {
@@ -14,24 +9,23 @@ function showRocket() {
 setTimeout(showRocket, 18000);
 
 
-/*
 planets.forEach(planet => {
     planet.addEventListener('click', () => {
         removeActiveClasses()
-        modal.classList.add('active');
+        planet.classList.add('modal')
     })
 })
 
 function removeActiveClasses() {
     planets.forEach(planet => {
-        planet.classList.remove('active')
+        planet.classList.remove('modal')
     })
 }
 
-removeModal.addEventListener('click', () => {
-    modal.classList.remove('active');
-})
 
-const para = document.createElement('p');
-para.textContent = "Neptune is the eighth and farthest planet from the Sun. It is the fourth-largest planet in the Solar System by diameter, the third-most-massive planet, and the densest giant planet. It is 17 times the mass of Earth, and slightly more massive than its near-twin Uranus.";
-Neptune.append('para'); */
+
+
+
+
+
+
